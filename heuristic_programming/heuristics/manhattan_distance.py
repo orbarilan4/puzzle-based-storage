a827a3e23069a219cd5a72db9f511c1cce2b69bf
Or Bar-Ilan <orbarilan4@gmail.com>
2020-01-11 21:08:20 +0200

    some bugs fixed

--- heuristic_programming/heuristics/manhattan_distance.py
+++ heuristic_programming/heuristics/manhattan_distance.py
@@ -1,22 +1,32 @@
 from heuristic_programming.utils.grid_utils import get_grid_loads
 
 
-# manhattan_distance is function which looking for the closest pairs <some load location,extraction point>
-# and return the distance between them
+# manhattan_distance_pair is function which looking for the closest pairs <some load location,extraction point>
+# and return the distance between them and the pair closest_load, closest_extraction_point
 # Example (when there are extraction points at [2,1],[2,2]).
 # p x p
 # p x p
 # p e p
-# The function will return : 1 . because the closest pair: [1,1] - load point, [2,1] - extraction point
-def manhattan_distance(self, state):
+# The function will return: minimum_distance = 1. because the closest pair: [1,1] - load point, [2,1] - extraction point
+#                           closest_load = [1,1]
+#                           closest_extraction_point = [2,1]
+def manhattan_distance_pairs(state):
     loads_location = get_grid_loads(state.grid)
     minimum_distance = float("inf")
     for load_location in loads_location:
         for extraction_point in state.extraction_points:
             distance = abs(load_location[0] - extraction_point[0]) +\
                        abs(load_location[1] - extraction_point[1])
-        if distance < minimum_distance:
-            minimum_distance = distance
+            if distance < minimum_distance:
+                minimum_distance = distance
+                closest_load = load_location
+                closest_extraction_point = extraction_point
+    return minimum_distance, closest_load, closest_extraction_point
+
+
+# This function will return only the minimum_distance parameter from manhattan_distance_pairs function
+def manhattan_distance(self, state):
+    minimum_distance, _, _ = manhattan_distance_pairs(state)
     return minimum_distance
 
 
--- heuristic_programming/run.py
+++ heuristic_programming/run.py
@@ -12,7 +12,7 @@ import numpy as np
 # Generate basic state
 def generate_state(grid_rows_number, grid_cols_number, load_points, extraction_points):
     grid = np.full((grid_rows_number, grid_cols_number), LOAD)
-    for i in range(grid_cols_number):
+    for i in range(grid_rows_number):
         for j in range(grid_cols_number):
             # If it is not a 'load point' it will be 'escort' or 'package'
             if [i, j] not in load_points:
@@ -23,30 +23,36 @@ def generate_state(grid_rows_number, grid_cols_number, load_points, extraction_p
 
 def main():
     start_states = []
-    for i in range(5):
-        start_states.append(generate_state(3, 3, [[0, 0]], [[2, 2]]))
+    for i in range(100):
+        start_states.append(generate_state(2, 4, [[1, 1]], [[0, 0], [0, 3]]))
 
     sum_of_developed_states = 0
     sum_of_close_lists = 0
+    sum_of__path_lengths = 0
     for start_state in start_states:
         print(start_state.grid)
         path, number_of_developed_states, close_list_size =\
             a_star(start_state, create_all_final_states(start_state), Heuristic(manhattan_distance))
+        sum_of__path_lengths += len(path)
         sum_of_developed_states += number_of_developed_states
         sum_of_close_lists += close_list_size
-    print("the avg of developed states ever in open list: " + str(sum_of_developed_states / 5))
-    print("the avg of close list: " + str(sum_of_close_lists / 5))
+    print("the sum of the path lengths: "+ str(sum_of__path_lengths))
+    print("the avg of developed states ever in open list: " + str(sum_of_developed_states / 100))
+    print("the avg of close list: " + str(sum_of_close_lists / 100))
 
     sum_of_developed_states = 0
     sum_of_close_lists = 0
+    sum_of__path_lengths = 0
     for start_state in start_states:
         print(start_state.grid)
         path, number_of_developed_states, close_list_size =\
             a_star(start_state, create_all_final_states(start_state), Heuristic(zero_dummy))
+        sum_of__path_lengths += len(path)
         sum_of_developed_states += number_of_developed_states
         sum_of_close_lists += close_list_size
-    print("the avg of developed states ever in open list: " + str(sum_of_developed_states / 5))
-    print("the avg of close list: " + str(sum_of_close_lists / 5))
+    print("the sum of the path lengths: "+ str(sum_of__path_lengths))
+    print("the avg of developed states ever in open list: " + str(sum_of_developed_states / 100))
+    print("the avg of close list: " + str(sum_of_close_lists / 100))
 
 
 if __name__ == '__main__':
