8f6e26c3565e13f9945b6ec95d94180838c2423a
Or Bar-Ilan <orbarilan4@gmail.com>
2020-01-15 01:49:24 +0200

    manhattan distance considering blocks heuristic added

--- heuristic_programming/run.py
+++ heuristic_programming/run.py
@@ -1,10 +1,12 @@
 from heuristic_programming.state import State
 from heuristic_programming.heuristics.heuristic import Heuristic
 from heuristic_programming.heuristics.manhattan_distance import manhattan_distance
+from heuristic_programming.heuristics.manhattan_distance_plus_blocks import manhattan_distance_plus_blocks
 from heuristic_programming.heuristics.zero_dummy import zero_dummy
 from heuristic_programming.states.final_states import create_all_final_states
 from heuristic_programming.a_star import a_star
 import random
+import time
 from settings import *
 import numpy as np
 
@@ -23,36 +25,64 @@ def generate_state(grid_rows_number, grid_cols_number, load_points, extraction_p
 
 def main():
     start_states = []
-    for i in range(100):
+    iterations_number = int(ITERATIONS_NUMBER)
+    for i in range(iterations_number):
         start_states.append(generate_state(2, 4, [[1, 1]], [[0, 0], [0, 3]]))
+    print(ITERATIONS_NUMBER + " start states created, each one of them is an iteration")
 
+    print("\nResults for A* Algorithm: (with manhattan distance considering blocks heuristic)")
+    print("================================================================================")
+    start_time = time.time()
     sum_of_developed_states = 0
     sum_of_close_lists = 0
     sum_of__path_lengths = 0
     for start_state in start_states:
-        print(start_state.grid)
+        # print(start_state.grid)
+        path, number_of_developed_states, close_list_size = \
+            a_star(start_state, create_all_final_states(start_state), Heuristic(manhattan_distance_plus_blocks))
+        sum_of__path_lengths += len(path)
+        sum_of_developed_states += number_of_developed_states
+        sum_of_close_lists += close_list_size
+    print("the sum of the path lengths for all iterations: " + str(sum_of__path_lengths))
+    print("the avg of developed states ever in open-list: " + str(sum_of_developed_states / iterations_number))
+    print("the avg of the close-list size: " + str(sum_of_close_lists / iterations_number))
+    print("the CPU time is: " + str(time.time() - start_time))
+
+    print("\nResults for A* Algorithm: (with manhattan distance heuristic)")
+    print("================================================================================")
+    start_time = time.time()
+    sum_of_developed_states = 0
+    sum_of_close_lists = 0
+    sum_of__path_lengths = 0
+    for start_state in start_states:
+        #print(start_state.grid)
         path, number_of_developed_states, close_list_size =\
             a_star(start_state, create_all_final_states(start_state), Heuristic(manhattan_distance))
         sum_of__path_lengths += len(path)
         sum_of_developed_states += number_of_developed_states
         sum_of_close_lists += close_list_size
-    print("the sum of the path lengths: "+ str(sum_of__path_lengths))
-    print("the avg of developed states ever in open list: " + str(sum_of_developed_states / 100))
-    print("the avg of close list: " + str(sum_of_close_lists / 100))
+    print("the sum of the path lengths for all iterations: " + str(sum_of__path_lengths))
+    print("the avg of developed states ever in open-list: " + str(sum_of_developed_states / iterations_number))
+    print("the avg of the close-list size: " + str(sum_of_close_lists / iterations_number))
+    print("the CPU time is: " + str(time.time() - start_time))
 
+    print("\nBFS Algorithm: (A* with zero dummy heuristic)")
+    print("================================================================================")
+    start_time = time.time()
     sum_of_developed_states = 0
     sum_of_close_lists = 0
     sum_of__path_lengths = 0
     for start_state in start_states:
-        print(start_state.grid)
+        #print(start_state.grid)
         path, number_of_developed_states, close_list_size =\
             a_star(start_state, create_all_final_states(start_state), Heuristic(zero_dummy))
         sum_of__path_lengths += len(path)
         sum_of_developed_states += number_of_developed_states
         sum_of_close_lists += close_list_size
-    print("the sum of the path lengths: "+ str(sum_of__path_lengths))
-    print("the avg of developed states ever in open list: " + str(sum_of_developed_states / 100))
-    print("the avg of close list: " + str(sum_of_close_lists / 100))
+    print("the sum of the path lengths for all iterations: " + str(sum_of__path_lengths))
+    print("the avg of developed states ever in open-list: " + str(sum_of_developed_states / iterations_number))
+    print("the avg of the close-list size: " + str(sum_of_close_lists / iterations_number))
+    print("the CPU time is: " + str(time.time() - start_time))
 
 
 if __name__ == '__main__':
