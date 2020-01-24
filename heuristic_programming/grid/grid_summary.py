28d3405b2f9917a4d0ce2adba81dde62b8ea4265
Or Bar-Ilan <orbarilan4@gmail.com>
2020-01-24 21:08:12 +0200

    Fix create_all_final_states fucntion (redefine final state)

--- heuristic_programming/grid/grid_summary.py
+++ heuristic_programming/grid/grid_summary.py
@@ -1,13 +1,15 @@
-from settings import PACKAGE, ESCORT
+from settings import PACKAGE, ESCORT, LOAD
 
 
 # For a given grid it finds the amount of escorts (marked in ESCORT) and packages (marked in PACKAGE)
 def get_grid_summary(grid):
-    summary = {PACKAGE: 0, ESCORT: 0}
+    summary = {PACKAGE: 0, ESCORT: 0, LOAD: 0}
     for row in range(0, grid.shape[0]):
         for col in range(0, grid.shape[1]):
             if grid[row, col] == PACKAGE:
                 summary[PACKAGE] += 1
             if grid[row, col] == ESCORT:
                 summary[ESCORT] += 1
+            if grid[row, col] == LOAD:
+                summary[LOAD] += 1
     return summary
--- heuristic_programming/states/final_states.py
+++ heuristic_programming/states/final_states.py
@@ -6,33 +6,39 @@ from settings import *
 
 
 # For a given state (grid and his extractions points) it will generate all the optional final states
-# Final state is defined by the location of the requested Package (Load)
-# if the Load is in the I/0 point it is a final state
+# Final state is defined by the location of the escorts and zero number of requested loads
+# There is at least one Extraction Point with escort in final state (and there is no loads on the grid)
 def create_all_final_states(state):
     summary = get_grid_summary(state.grid)
-    grid_size_without_extraction_points = state.grid.size - len(state.extraction_points)
+    # Final state has no loads (requested loads will become escorts)
+    summary[ESCORT] += summary[LOAD]
+    del summary[LOAD]
+
     combinations = []
     final_states = []
+    # It will find all the grid combinations for the a summary
+    # Example: s = {'p': 2, 'e': 1} => combinations are 'epp' or 'pep' or 'ppe'
+    for bits in it.combinations(range(state.grid.size), summary[ESCORT]):
+        s = [PACKAGE] * state.grid.size
+        for bit in bits:
+            s[bit] = ESCORT
+        combinations.append(''.join(s))
 
-    # For each extraction point
-    for extraction_point in state.extraction_points:
-        # It will find all the grid combinations for the a summary
-        # Example: s = {'p': 2, 'e': 1} => combinations are 'epp' or 'pep' or 'ppe'
-        for bits in it.combinations(range(state.grid.size-1), summary[ESCORT]):
-            s = [PACKAGE] * (state.grid.size-1)
-            for bit in bits:
-                s[bit] = ESCORT
-            # Add load (x) to the extraction point location and save it
-            extraction_point_location = extraction_point[0] * state.grid.shape[1] + (extraction_point[1])
-            s.insert(extraction_point_location, LOAD)
-            combinations.append(''.join(s))
-
-    # Each combination will be a final state
+    # Each combination will be a final state (only if there is at least one extraction point empty)
     for combination in combinations:
         # Combination becomes a grid
         combination = np.array(list(combination))
         combination = np.reshape(combination, (-1, state.grid.shape[1]))
         # Create new state from combination and add it to s0
         new_final_state = State(combination, state.extraction_points, None)
-        final_states.append(new_final_state)
+        if is_there_empty_extraction_point(new_final_state):
+            final_states.append(new_final_state)
     return final_states
+
+
+# If there is at least one extraction point empty (contains ESCORT) return true else false
+def is_there_empty_extraction_point(state):
+    for extraction_point in state.extraction_points:
+        if state.grid[extraction_point[0], extraction_point[1]] == ESCORT:
+            return True
+    return False
